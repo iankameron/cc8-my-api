6f4b0ecab9b9516129fdd9107157fba662d02089
Ian Cameron <ian.kameron@gmail.com>
2019-04-27 11:34:22 +0900

    setting up for testing. abstracted server to server.js

--- routes/api/center/index.js
+++ routes/api/center/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (services) => {
-  
+
+  router.get("", (req, res) => {
+    console.log("gettingin");
+    res.json({center: "this is a list of centers."});
+  });
+
   return router;
 }
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,26 @@
-const {expect} = require("mocha");
+const chaiHttp = require("chai-http");
+const chai = require("chai");
+chai.use(chaiHttp);
+chai.should();
 
-describe("Test Placeholder", () => {
-  it("should test something", () => {
-    expect(true).to.equal(true);
+const { server } = require("../server");
+//const { pokemon, attacks, types } = require("../src/data/index.js");
+
+//const _ = require("underscore");
+const sinon = require("sinon");
+describe("API tests", () => {
+  let request;
+  beforeEach(() => {
+    request = chai.request(server).keepOpen();
+  });
+  afterEach(() => {
+    request.close();
+  });
+  describe("Test Placeholder", () => {
+    it("should test something", async () => {
+      const res = await request.get("/api/centers");
+      console.log(res.body);
+      true.should.equal(true);
+    });
   });
 });
