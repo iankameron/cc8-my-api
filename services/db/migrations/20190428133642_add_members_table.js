e8287fe3d40280f697a390616b5b2d1c2b83addf
Ian Cameron <ian.kameron@gmail.com>
2019-04-28 13:43:43 +0900

    created members table

---
+++ services/db/migrations/20190428133642_add_members_table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("members", t => {
+    // columns
+    t.increments().index();
+    t.string("name", 100).notNullable();
+    t.integer("center_id").notNullable();
+    t.integer("skill_level");
+    // foreign keys
+    t.foreign("center_id")
+      .references("id")
+      .inTable("centers");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable("members");
+};
